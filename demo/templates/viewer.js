3391b910a3f43329763f7ae4f7504a2eeb30d301
Stéphane Brunner <sbrunner@prospection-geospatial-demo.dmz.lsn.camptocamp.com>
2013-03-27 19:17:42 +0100

    Test on demo server

--- demo/templates/viewer.js
+++ demo/templates/viewer.js
@@ -4,7 +4,6 @@ Ext.onReady(function() {
      */
     // OpenLayers
     OpenLayers.Number.thousandsSeparator = ' ';
-    OpenLayers.IMAGE_RELOAD_ATTEMPTS = 5;
     OpenLayers.DOTS_PER_INCH = 72;
     OpenLayers.ProxyHost = "${request.route_url('ogcproxy')}?url=";
 
@@ -253,13 +252,6 @@ Ext.onReady(function() {
              actionConfig: {
                 tooltip: OpenLayers.i18n('Open Google Earth Panel')
              }
-        }, {
-             ptype: 'cgxp_streetview',
-             actionTarget: 'center.tbar',
-             outputTarget: 'center',
-             toggleGroup: 'maptools',
-             tooltipText: OpenLayers.i18n('StreetView'),
-             baseURL: "${request.static_url('demo:static/lib/cgxp/geoext.ux/ux/StreetViewPanel/')}"
         }, {
             ptype: "cgxp_fulltextsearch",
             url: "${request.route_url('fulltextsearch', path='')}",
