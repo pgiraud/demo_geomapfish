a819aa271325dbb0ee3d42ad46a37f3a932b4b53
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-02-04 13:35:52 +0100

    Update c2cgeoportal

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from pyramid.config import Configurator
-from c2cgeoportal import locale_negotiator, add_interface, INTERFACE_TYPE_SENCHA_TOUCH
-from c2cgeoportal.resources import FAModels
+from c2cgeoportal import locale_negotiator, \
+    add_interface, INTERFACE_TYPE_SENCHA_TOUCH, \
+    add_admin_interface, add_static_view
 from c2cgeoportal.lib.authentication import create_authentication
 from demo.resources import Root
 
@@ -16,33 +17,18 @@ def main(global_config, **settings):
         authentication_policy=create_authentication(settings)
     )
 
-    config.add_settings({'srid': 3857})
-
     config.include('c2cgeoportal')
 
     config.add_translation_dirs('demo:locale/')
 
-    from pyramid.settings import asbool
-    if asbool(config.get_settings().get('enable_admin_interface')):
-        config.formalchemy_admin(
-            'admin', package='demo',
-            view='fa.jquery.pyramid.ModelView', factory=FAModels
-        )
-
-    config.add_route('checker_all', '/checker_all')
-
     # scan view decorator for adding routes
     config.scan()
-
-    # add the main static view
-    config.add_static_view(
-        'proj', 'demo:static',
-        cache_max_age=int(config.get_settings()["default_max_age"])
-    )
 
     add_interface(config)
     add_interface(config, 'edit')
     add_interface(config, 'routing')
     add_interface(config, 'mobile', INTERFACE_TYPE_SENCHA_TOUCH)
+    add_admin_interface(config, "demo")
+    add_static_view(config, "demo")
 
     return config.make_wsgi_app()
